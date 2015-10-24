d2867df7e0bd2e8d7e37d3a99e5fbf389503998a
Shide93 <chen93@mail.ru>
2015-10-24 16:27:57 +0400

    Created service template

--- app/src/main/java/com/example/nb/hakaton/MainActivity.java
+++ app/src/main/java/com/example/nb/hakaton/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.nb.hakaton;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,23 +10,30 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.nb.hakaton.service.MainService;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        //запуск сервиса
+
+
+    }
+    public void onClickStart(View v) {
+        startService(new Intent(this, MainService.class));
+    }
+
+    public void onClickStop(View v) {
+       stopService(new Intent(this, MainService.class));
     }
 
     @Override
